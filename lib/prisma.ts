79095a5ec0c47dc6d8e8aba7233e8db4c45f7111
srivishnu25 <thatisrivishnu@gmail.com>
2024-09-27 21:08:03 +0530

    prisma set-up

---
+++ lib/prisma.ts
@@ -0,0 +1,27 @@
+import ws from 'ws'
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+
+const prismaClientSingleton = () => {
+  neonConfig.webSocketConstructor = ws
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+  return new PrismaClient({
+    adapter: new PrismaNeon(pool),
+  })
+}
+
+// Update the global declaration
+declare global {
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+}
+
+// Use a fallback for globalThis if it's not available
+const globalObj: typeof globalThis =
+  typeof globalThis !== 'undefined' ? globalThis : global
+
+export const prisma = globalObj.prismaGlobal || prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalObj.prismaGlobal = prisma
